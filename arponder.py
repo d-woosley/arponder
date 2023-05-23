554c9a362cfba765dabba574f913e0415e358a95
a2trdm <115438340+a2trdm@users.noreply.github.com>
2023-05-23 13:35:00 -0500

    Update arponder.py
    Corrected ASCII logo colors

--- arponder.py
+++ arponder.py
@@ -30,12 +30,12 @@ class main():
 
     def ascii(self):
         print(Fore.LIGHTRED_EX + "   _____ ____________________ " + Fore.LIGHTBLACK_EX + "                _           " + Fore.RESET)
-        print(Fore.LIGHTRED_EX + "  /  _  \\\\______   \\______   \\" + Fore.LIGHTBLACK_EX + "               | |          "+ Fore.RESET)
-        print(Fore.LIGHTRED_EX + " /  /_\  \|       _/|     ___/" + Fore.LIGHTBLACK_EX + " ___  _ __   __| | ___ _ __ "+ Fore.RESET)
-        print(Fore.LIGHTRED_EX + "/    |    \    |   \|    |" + Fore.LIGHTBLACK_EX + "    / _ \| '_ \ / _` |/ _ \ '__|"+ Fore.RESET)
-        print(Fore.LIGHTRED_EX + "\\____|__  /____|_  /|____|" + Fore.LIGHTBLACK_EX + "   | (_) | | | | (_| |  __/ |   "+ Fore.RESET)
-        print(Fore.LIGHTRED_EX + "        \\/       \\/" + Fore.LIGHTBLACK_EX + "           \___/|_| |_|\__,_|\___|_|    "+ Fore.RESET)
-        print(Fore.LIGHTWHITE_EX + "___________________________________________________________")
+        print(Fore.LIGHTRED_EX + "  /  _  \\\\______   \\______   \\" + Fore.LIGHTBLACK_EX + "               | |          " + Fore.RESET)
+        print(Fore.LIGHTRED_EX + " /  /_\  \|       _/|     ___/" + Fore.LIGHTBLACK_EX + " ___  _ __   __| | ___ _ __ " + Fore.RESET)
+        print(Fore.LIGHTRED_EX + "/    |    \    |   \|    |" + Fore.LIGHTBLACK_EX + "    / _ \| '_ \ / _` |/ _ \ '__|" + Fore.RESET)
+        print(Fore.LIGHTRED_EX + "\\____|__  /____|_  /|____|" + Fore.LIGHTBLACK_EX + "   | (_) | | | | (_| |  __/ |   " + Fore.RESET)
+        print(Fore.LIGHTRED_EX + "        \\/       \\/" + Fore.LIGHTBLACK_EX + "           \___/|_| |_|\__,_|\___|_|    " + Fore.RESET)
+        print(Fore.LIGHTWHITE_EX + "___________________________________________________________" + Fore.RESET)
         return
 
     def getArgs(self):
